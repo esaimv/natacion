fd45d3350cc3c74352843ee9bb7149be68bd647c
esaimv <esaimv@gmail.com>
2017-05-21 22:25:22 +0000

    Nav y menus terminados

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,35 @@
 
 $(window).load(function(){
+  Width = $(window).width();
+  if(Width>970){
     $(".submenu > a").trigger("click");
+  }
     $(".subsubmenu > a").trigger("click");
     $(".subsubmenu > a").trigger("click");
 });
 
 $(document).ready(function(){
+  Width = $(window).width();
+  if(Width<=970){
+    $("#logo-ith").attr("width", "130%")
+    if($(".submenu > a").parent("li").hasClass("open")) {
+        $(".submenu > a").trigger("click");
+      }
+  }else{
+    $("#logo-ith").attr("width", "100%")
+    if(!($(".submenu > a").parent("li").hasClass("open"))) {
+        $(".submenu > a").trigger("click");
+      }
+  }
   $(window).resize(function() {
     Width = $(window).width();
-    if( Width <=  970 ) {
+    if( Width <=  970 ) { 
+      $("#logo-ith").attr("width", "130%")     
       if($(".submenu > a").parent("li").hasClass("open")) {
         $(".submenu > a").trigger("click");
       }
     }else{
+      $("#logo-ith").attr("width", "100%")
       if(!($(".submenu > a").parent("li").hasClass("open"))) {
         $(".submenu > a").trigger("click");
       }
